313e932d7da73c68bfee2a3c40a2baac12e50021
seriesninefournine <78902612+seriesninefournine@users.noreply.github.com>
2021-12-07 02:09:49 +0500

    script to up devstack

---
+++ openstack_up.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+#dnf -y install
+sudo su
+#apt -y purge python3-simplejson
+#apt -y autoremove
+
+
+
+useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/stack
+sudo -u stack -i
+
+
+
+
+#git clone https://opendev.org/openstack/devstack.git
+git clone https://opendev.org/openstack/devstack.git -b stable/xena
+
+sed -i -e 's/$cmd_pip $upgrade/$cmd_pip $upgrade --ignore-installed/g' ./devstack/inc/python 
+cd devstack
+
+
+echo '# Sample ``local.conf`` for user-configurable variables in ``stack.sh``
+
+[[local|localrc]]
+
+ADMIN_PASSWORD=12345678
+DATABASE_PASSWORD=stackdb
+RABBIT_PASSWORD=stackqueue
+SERVICE_PASSWORD=$ADMIN_PASSWORD
+
+IP_VERSION=4
+SERVICE_IP_VERSION=4
+HOST_IP=10.1.2.10
+
+LOGFILE=$DEST/logs/stack.sh.log
+
+LOG_COLOR=False' > local.conf
+
+./stack.sh
